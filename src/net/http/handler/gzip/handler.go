cc83ab83b20384b1e28246e57969ba71b5c62c1f
Joao da Silva <silvajoao@gmail.com>
2012-04-03 16:03:14 +0200

    Use os.Create instead of os.OpenFile

--- src/net/http/handler/gzip/handler.go
+++ src/net/http/handler/gzip/handler.go
@@ -19,7 +19,7 @@ type File interface {
 // Extends net.http.FileSystem to also support opening files to write.
 type FileSystem interface {
   http.FileSystem
-	OpenFile(name string, flag int, perm os.FileMode) (File, error)
+  Create(name string) (File, error)
 }
 
 // Dir implements FileSystem using the native file system restricted to a
@@ -49,10 +49,10 @@ func (d Dir) Open(name string) (http.File, error) {
   return nil, err
 }
 
-func (d Dir) OpenFile(name string, flag int, perm os.FileMode) (File, error) {
+func (d Dir) Create(name string) (File, error) {
   file, err := d.getFileName(name)
   if err == nil {
-    f, err := os.OpenFile(file, flag, perm)
+    f, err := os.Create(file)
     if err == nil {
       return f, nil
     }
@@ -60,8 +60,18 @@ func (d Dir) OpenFile(name string, flag int, perm os.FileMode) (File, error) {
   return nil, err
 }
 
+type gzipFileHandler struct {
+  root FileSystem
+}
+
+func (h *gzipFileHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  // TODO
+}
+
+// Similar to net.http.FileServer, but gzips content on the fly and serves it
+// encoded, when supported by the client.
 func FileServer(root FileSystem) http.Handler {
-  return nil
+  return &gzipFileHandler{root}
 }
 
 func Foo() {
